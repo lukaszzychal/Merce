d8e2f5d15a4acda285df873263595f4f062966ce
Lukasz Zychal <lukaszzpriv@gmail.com>
2023-01-26 14:11:29 +0100

    enable declare strict type

--- tests/Application/GetUseCaseTest.php
+++ tests/Application/GetUseCaseTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Application;
 
 use App\Client\CurlClient;
--- tests/Provider/ProviderData.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Tests\Provider;
-
-class ProviderData
-{
-    public const HOST = 'http://localhost';
-}
