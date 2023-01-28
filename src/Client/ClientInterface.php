2e7ff8602e7aee1359ac751dd992cc4a73707d40
Lukasz Zychal <lukaszzpriv@gmail.com>
2023-01-28 22:45:28 +0100

    add decalre rest method http

--- src/Client/ClientInterface.php
+++ src/Client/ClientInterface.php
@@ -1,8 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Client;
+
+use Psr\Http\Message\ResponseInterface;
 
 interface ClientInterface
 {
-    
+    public function get(string $uri, array $options = []): ResponseInterface;
+ 
+    // public function post(string $uri, array $options = []): ResponseInterface;
+    // public function put(string $uri, array $options = []): ResponseInterface;
+    // public function path(string $uri, array $options = []): ResponseInterface;
+    // public function delete(string $uri, array $options = []): ResponseInterface;
+    // public function options(string $uri, array $options = []): ResponseInterface;
+    // public function trace(string $uri, array $options = []): ResponseInterface;
+    // public function connect(string $uri, array $options = []): ResponseInterface;
+  
+
 }
