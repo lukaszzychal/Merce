800697e4bc69b0afce9a79a8d6d25d959a3cfeed
Lukasz Zychal <lukaszzpriv@gmail.com>
2023-01-28 17:55:47 +0100

    add headers test data

--- tests/Client/Provider/ShareData.php
+++ tests/Client/Provider/ShareData.php
@@ -5,4 +5,19 @@ namespace App\Tests\Client\Provider;
 class ShareData
 {
     public const HOST = 'http://localhost';
+
+    public const HEADER_ACCEPT = [
+        'key' =>'Accept',
+        'value' => 'text/html'
+    ];
+    
+    public const HEADER_ACCEPT_LANGUAGE = [
+        'key' =>'Accept-Language',
+        'value' => 'en-US'
+    ];
+    public const HEADERS =  [
+        self::HEADER_ACCEPT['key'] =>  self::HEADER_ACCEPT['value'],
+        self::HEADER_ACCEPT_LANGUAGE['key'] =>  self::HEADER_ACCEPT_LANGUAGE['value']           
+    ];
+
 }
