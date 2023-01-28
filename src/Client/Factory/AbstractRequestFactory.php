ddae75ef15b9d1483dbe5275df47b5853064941f
Lukasz Zychal <lukaszzpriv@gmail.com>
2023-01-28 22:48:16 +0100

    add new method create reqest with heder

--- src/Client/Factory/AbstractRequestFactory.php
+++ src/Client/Factory/AbstractRequestFactory.php
@@ -7,8 +7,9 @@ namespace App\Client\Factory;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
 
-abstract class AbstractRequestFactory implements RequestFactoryInterface
+abstract class AbstractRequestFactory implements  RequestFactoryInterface
 {
     public function __construct(
         protected ?RequestFactoryInterface $requestFactory = null
@@ -19,5 +20,25 @@ abstract class AbstractRequestFactory implements RequestFactoryInterface
     public function createRequest(string $method, $uri): RequestInterface
     {
         return $this->requestFactory->createRequest($method, $uri);
+    }
+
+    public function createRequestWithHeaders(
+        string $method,
+        string $uri,
+        array $headers = []
+    ): RequestInterface
+    {
+        $request = $this->createRequest($method, $uri);
+        $request = $this->addHeadersToRequest($request, $headers);
+        return $request;
+    }
+
+    public function addHeadersToRequest(RequestInterface $request, array $headers = []): RequestInterface
+    {
+        foreach ($headers as $header => $value) {
+           $request =  $request->withAddedHeader($header, $value);
+        }
+
+        return $request;
     }
 }
