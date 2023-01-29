2677be9ab31ebeaed9ae6eefa82ff495e8dd1dd6
Lukasz Zychal <lukaszzpriv@gmail.com>
2023-01-29 18:08:37 +0100

    feature: impl basic curl code

--- src/Client/CurlClient.php
+++ src/Client/CurlClient.php
@@ -12,135 +12,54 @@ class CurlClient extends AbstractClient
 
      public function execute(RequestInterface $request): ResponseDTO
     {
-    //     $defaults_options = [
-    //         CURLOPT_URL => (string) $request->getUri(),
+        $defaults_options = [
+            CURLOPT_URL => (string) $request->getUri(),
 
-    //         CURLOPT_HEADER => 0,
+            CURLOPT_HEADER => 0,
     
-    //         CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
     
-    //         CURLOPT_TIMEOUT => 4
-    //     ];
+            CURLOPT_TIMEOUT => 4
+        ];
         
 
-    // $ch = curl_init();
-    // curl_setopt_array($ch, $defaults_options);
-    // $result = curl_exec($ch);
-    // if($result === false){
-    //         throw new NetworkException(
-    //             $request, 
-    //             'Client curl can not return resoult. Try again.',
-    //             HttpStatus::BAD_REQUEST
-    //         );
-    // }
-    // curl_close($ch);
-    //     // var_dump($result);
-        // die();
+    $handle = curl_init();
+    if(!$handle){
+        throw new NetworkException(
+            $request, 
+            'Client curl can not return resoult. Try again.',
+            HttpStatus::BAD_REQUEST
+        );
+}
+    curl_setopt($handle, CURLINFO_HEADER_OUT, true);
+    curl_setopt_array($handle, $defaults_options);
+    $result = curl_exec($handle);
+    if($result === false){
+            throw new NetworkException(
+                $request, 
+                'Client curl can not return resoult. Try again.',
+                HttpStatus::BAD_REQUEST
+            );
+    }
+    
+        $http_code = curl_getinfo($handle, CURLINFO_RESPONSE_CODE);
+
+        $http_headers = curl_getinfo($handle, CURLINFO_HEADER_OUT);
+   
+        curl_close($handle);
+
         return new ResponseDTO(
-            200,
-             'any Reason Phrase ',
+            $http_code,
+             ' ',
               $result, 
-               [
-                'content-type' => 'aplication/json'
-            ]
+               $this->parseHeadersCurl($http_headers)
         );
     }
   
-    function curl_post($url, array $post = NULL, array $options = array())
-
-{
-
-    $defaults = array(
-
-        CURLOPT_POST => 1,
-
-        CURLOPT_HEADER => 0,
-
-        CURLOPT_URL => $url,
-
-        CURLOPT_FRESH_CONNECT => 1,
-
-        CURLOPT_RETURNTRANSFER => 1,
-
-        CURLOPT_FORBID_REUSE => 1,
-
-        CURLOPT_TIMEOUT => 4,
-
-        CURLOPT_POSTFIELDS => http_build_query($post)
-
-    );
-
-
-
-    $ch = curl_init();
-
-    curl_setopt_array($ch, ($options + $defaults));
-
-    if( ! $result = curl_exec($ch))
-
+    private function parseHeadersCurl(string|bool $http_headers): array 
     {
-
-        trigger_error(curl_error($ch));
-
+        return []; // @todo to do later
     }
-
-    curl_close($ch);
-
-    return $result;
-
-}
-
-
-
-/**
-
- * Send a GET requst using cURL
-
- * @param string $url to request
-
- * @param array $get values to send
-
- * @param array $options for cURL
-
- * @return string
-
- */
-
-function curl_get($url, array $get = NULL, array $options = [])
-
-{    
-
-    $defaults = array(
-
-        CURLOPT_URL => $url,
-
-        CURLOPT_HEADER => 0,
-
-        CURLOPT_RETURNTRANSFER => TRUE,
-
-        CURLOPT_TIMEOUT => 4
-
-    );
-
-    
-
-    $ch = curl_init();
-
-    curl_setopt_array($ch, ($options + $defaults));
-
-    if( ! $result = curl_exec($ch))
-
-    {
-
-        trigger_error(curl_error($ch));
-
-    }
-
-    curl_close($ch);
-
-    return $result;
-
-}
 
 
 }
