3e22542282ca498da7a827929c8cc8084bb1977c
Lukasz Zychal <lukaszzpriv@gmail.com>
2023-01-29 19:03:09 +0100

    test: add markTest

--- tests/Client/Application/DeleteUseCaseTest.php
+++ tests/Client/Application/DeleteUseCaseTest.php
@@ -10,26 +10,10 @@ class DeleteUseCaseTest extends BaseTestCase
     public function testGiveCurlClientWhenSendDeleteMethodaReturnValidResposne(): void
     {
         $this->markTestSkipped("Not Implemetation. ToDo");
-        $resposne = $this->client->delete('http://localhost/users/1', [
-            'headers' => [
-                'content-type' => 'aplication/json'
-            ]
-        ]);
-
-        $this->assertInstanceOf(ResposneInterface::class, $resposne);
-        $this->assertSame(200, $resposne->getStatus());
     }
 
     public function testGiveCurlClientWhenSendDeleteMethodReturnInValidResposne(): void
     {
         $this->markTestSkipped("Not Implemetation. ToDo");
-        $resposne = $this->client->delete('http://localhost/users/1', [
-            'headers' => [
-                'content-type' => 'aplication/json'
-            ]
-        ]);
-
-        $this->assertInstanceOf(ResposneInterface::class, $resposne);
-        $this->assertSame(400, $resposne->getStatus());
     }
 }
